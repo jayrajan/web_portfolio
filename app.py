af75fd70dd38c2b88778a6ceb6b6bfc12d6a2802
Jerin Rajan <jrajan@jprtech.co.uk>
2021-01-18 18:21:07 +0000

    feat: define title variable

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 @app.route('/')
 def main():
+    title = 'JR Home'
     name = 'Jay Rajan'
-    return render_template('home.html',name=name)
+    return render_template('home.html',name=name,title=title)
 
 if __name__== '__main__':
     app.run(debug=True)
